6d0b8d32c64562b9b0f18f6a0cfb606a0ce1421c
Puvli44 <pavel-zajtsev@yandex.ru>
2024-09-19 16:10:41 +0300

    added cors edit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:8080' ], 
+    origin: ['https://puvli.nomorepartiesco.ru'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
